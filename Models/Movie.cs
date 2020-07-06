495937507a30d7c50c8a84b4afca6cd598fd3f32
Jose Mª Izquierdo Galiot <igaliot21@gmail.com>
2020-07-06 20:37:01 +0200

    Getting Started

---
+++ Models/Movie.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    public class Movie
+    {
+        private int id;
+        private string name;
+        public Movie() { }
+        public Movie(string Name) {
+            this.name = Name;
+        }
+        public Movie(int Id, string Name)
+        {
+            this.id = Id;
+            this.name = Name;
+        }
+        public int Id {
+            get { return this.id;}
+            set { this.id = value;} 
+        }
+        public string Name {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+    }
+}
